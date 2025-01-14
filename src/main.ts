d4adf892844a551955aa52d1e0409e7fa163b021
Nicolas Rio <nicolas.rio42@gmail.com>
2025-01-14 19:22:00 +0100

    Lidar step completed

--- src/main.ts
+++ src/main.ts
@@ -13,10 +13,10 @@ if (mapantApiToken === undefined) {
   throw new Error("MAPANT_API_TOKEN environment variable not set.");
 }
 
-const mapantApiBaseUrl =
-  Deno.env.get("MAPANT_API_BASE_URL") ?? "https://mapant.fr/api";
+const mapantApiBaseUrl = Deno.env.get("MAPANT_API_BASE_URL") ??
+  "https://mapant.fr/api";
 
-getAndHandleNextJob();
+await getAndHandleNextJob();
 
 async function getAndHandleNextJob() {
   const nextJobResponse = await fetch(
@@ -26,7 +26,7 @@ async function getAndHandleNextJob() {
       headers: {
         Authorization: `Bearer ${mapantApiWorkerId}.${mapantApiToken}`,
       },
-    }
+    },
   );
 
   if (!nextJobResponse.ok) {
@@ -38,8 +38,9 @@ async function getAndHandleNextJob() {
 
   const rawJsonResponse = await nextJobResponse.json();
 
-  const parsedJsonResponse =
-    nextJobApiEndpointResponseSchema.safeParse(rawJsonResponse);
+  const parsedJsonResponse = nextJobApiEndpointResponseSchema.safeParse(
+    rawJsonResponse,
+  );
 
   if (!parsedJsonResponse.success) {
     console.error("Not expected next-job endpoint response");
@@ -54,31 +55,38 @@ async function getAndHandleNextJob() {
   }
 
   if (validResponse.type === "lidar") {
-    // Download Lidar file
     await createDirIfItDoenstExist("lidar-files");
 
     const outDirPath = join(
       "lidar-step",
-      `${validResponse.data.x}_${validResponse.data.y}`
+      `${validResponse.data.x}_${validResponse.data.y}`,
     );
 
     await createDirIfItDoenstExist("lidar-step");
     const fileName = validResponse.data.tileUrl.split("/").reverse()[0];
     const filePath = join("lidar-files", fileName);
+
+    console.log(`Downloading lidar file: ${fileName}`);
     await downloadFileToDisk(validResponse.data.tileUrl, filePath);
 
-    const command = new Deno.Command("cassini", {
-      args: ["lidar", filePath, "-o", outDirPath],
-    });
+    console.log("Running cassini lidar subcommand");
+    const exitCode = await runCassiniInDocker(
+      "lidar",
+      filePath,
+      "-o",
+      outDirPath,
+    );
 
-    // create subprocess and collect output
-    const { code, stdout, stderr } = await command.output();
-    if (code !== 0) {
-      console.log(new TextDecoder().decode(stderr));
-      return;
-    }
+    if (exitCode !== 0) return;
 
-    console.log(new TextDecoder().decode(stdout));
+    console.log("Compressing lidar step files");
+    const [archivePath, compressExitCode] = await compressDir(
+      outDirPath,
+      "lidar-step",
+      `${validResponse.data.x}_${validResponse.data.y}`,
+    );
+
+    if (compressExitCode !== 0) return;
   }
 
   if (validResponse.type === "render") {
@@ -88,31 +96,68 @@ async function getAndHandleNextJob() {
   }
 }
 
+async function runCassiniInDocker(...args: string[]): Promise<number> {
+  return executeCommand(
+    "docker",
+    "run",
+    "--rm",
+    "-v",
+    `${Deno.cwd()}:/app`,
+    "nicorio42/cassini",
+    ...args,
+  );
+}
+
+async function compressDir(
+  inputDirectoryPath: string,
+  outputDirectoryPath: string,
+  fileName: string,
+) {
+  const archiveFileName = fileName + ".tar.bz2";
+  const archivePath = join(outputDirectoryPath, archiveFileName);
+
+  return [
+    archivePath,
+    await executeCommand("tar", "-cjvf", archivePath, inputDirectoryPath),
+  ];
+}
+
+async function executeCommand(command: string, ...args: string[]) {
+  const cmd = new Deno.Command(command, { args });
+  const { code, stdout, stderr } = await cmd.output();
+
+  if (code !== 0) {
+    console.log(new TextDecoder().decode(stderr));
+  } else {
+    console.log(new TextDecoder().decode(stdout));
+  }
+
+  return code;
+}
+
 async function downloadFileToDisk(url: string, filePath: string) {
   const lifarFileResponse = await fetch(url);
 
-  if (!lifarFileResponse.ok || lifarFileResponse.body === null)
+  if (!lifarFileResponse.ok || lifarFileResponse.body === null) {
     throw new Error(`Failed to fetch file: ${url}`);
+  }
 
-  const file = await Deno.open(filePath, {
+  using file = await Deno.open(filePath, {
     create: true,
     write: true,
   });
 
   await lifarFileResponse.body.pipeTo(file.writable);
-  file.close();
 }
 
 async function createDirIfItDoenstExist(dirPath: string) {
   try {
     await Deno.lstat(dirPath);
-    console.log("Folder already exists.");
   } catch (error) {
     if (error instanceof Deno.errors.NotFound) {
       await Deno.mkdir(dirPath, { recursive: true });
-      console.log("Folder created successfully.");
     } else {
-      console.error("An error occurred:", error);
+      console.error(error);
     }
   }
 }
